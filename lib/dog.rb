fe1cb2ff6e5dd7f631c29dfd83ebc7e4098b4b3a
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-22 17:03:23 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def name=name
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=breed
+         @breed = breed #writes
+    end
+
+    def breed
+        @breed #reads
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def name=name #write
+        @name = name
+    end
+
+    def name #read
+        @name
+    end
+
+    def job=job
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
+end
